22dbb1805c67c4ae19a023fdea39218de9beccd7
TJ Talusan <tjtalusan@gmail.com>
2025-12-04 08:11:12 +0800

    Replace bcrypt with bcryptjs to fix Railway build error

--- reset-password.js
+++ reset-password.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 async function resetPassword() {
     const pool = new Pool({
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const config = require('../config');
 const db = require('../db');
 const axios = require('axios');
